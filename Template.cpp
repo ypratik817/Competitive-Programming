bee5f718d40dd80ae8c43b63d33c2e4f7d80eb83
Pratik Yadav <93317361+ypratik817@users.noreply.github.com>
2022-12-22 18:24:10 +0530

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,60 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cctype>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <iterator>
+#include <set>
+#include <exception>
+#include <cstdlib>
+#include <iomanip>
+#include <bitset>
+#include <cstdio>
+#include <chrono>
+#include <complex>
+#include <algorithm>
+#include <numeric>
+#include <limits.h>
+#include <list>
+#include <vector> 
+#include <unordered_map>
+#include <map>
+#include <queue>
+#define nline "\n"
+#define ll long long int
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector<vi>
+#define vvll vector<vll>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001
+#define all(c) c.begin(), c.end()
+#define mp(x, y) make_pair(x, y)
+#define mem(a, val) memset(a, val, sizeof(a))
+#define eb emplace_back
+#define f first
+// #define s second
+// iota(p.begin(), p.end(), 1);
+
+using namespace std;
+
+
+
+int main()
+{
+  int t;
+  cin >> t;
+  while(t--)
+  {
+      
+  }
+
+
+  return 0;
+}
